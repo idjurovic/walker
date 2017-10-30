a9361e92565ffe0ceb5171bd67befcdc7513c083
Unknown <isabella.djurovic@gmail.com>
2017-10-30 18:42:35 -0400

    event triggers work

--- Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -255,5 +255,11 @@ namespace UnityStandardAssets.Characters.FirstPerson
             }
             body.AddForceAtPosition(m_CharacterController.velocity*0.1f, hit.point, ForceMode.Impulse);
         }
+
+        private void OnTriggerEnter(Collider other) {
+            if (other.tag == "event") {
+                Debug.Log("entered");
+            }
+        }
     }
 }
